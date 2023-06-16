ac9db6c9977f830dc287a118cd8842f5ecc0522e
leejinwoo3 <vmfkdcm3@naver.com>
2023-06-16 18:09:10 +0900

    spring

---
+++ src/main/java/com/example/board2/controller/BoardController.java
@@ -0,0 +1,31 @@
+package com.example.board2.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class BoardController {
+	
+	@RequestMapping(value="/")//localhost로 접속
+	public String index() {
+		return "index";
+	}
+	
+	@RequestMapping(value="/created",method = RequestMethod.GET)//localhost로 접속
+	public String created() {
+		return "bbs/created";
+	}
+	@RequestMapping(value="/list",method = RequestMethod.GET)//localhost로 접속
+	public String list() {
+		return "bbs/list";
+	}
+	@RequestMapping(value="/article",method = RequestMethod.GET)//localhost로 접속
+	public String article() {
+		return "bbs/article";
+	}
+	@RequestMapping(value="/updated",method = RequestMethod.GET)//localhost로 접속
+	public String updated() {
+		return "bbs/updated";
+	}
+}
